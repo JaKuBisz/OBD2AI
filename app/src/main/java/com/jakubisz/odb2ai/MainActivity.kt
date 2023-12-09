496027be4583d04028423be34174f05150b64a64
Jakub Kubisz <89148912+JaKuBisz@users.noreply.github.com>
2023-12-09 14:49:58 +0100

    Created project main structure from Empty project template
    com.jakubisz.odb2ai

---
+++ app/src/main/java/com/jakubisz/odb2ai/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.jakubisz.odb2ai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jakubisz.odb2ai.ui.theme.ODB2AITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ODB2AITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ODB2AITheme {
+        Greeting("Android")
+    }
+}
