c88ed1f3154788808cfe6a1c8714a1de7f13bf4a
Jakub Kubisz <jakub.kubisz@rws.com>
2023-12-15 01:21:15 +0100

    OdbHelper implementation

--- app/src/main/java/com/jakubisz/obd2ai/ObdHelper.kt
+++ app/src/main/java/com/jakubisz/obd2ai/ObdHelper.kt
@@ -1,17 +1,46 @@
 package com.jakubisz.obd2ai
 
+import com.github.eltonvs.obd.command.control.PendingTroubleCodesCommand
+import com.github.eltonvs.obd.command.control.PermanentTroubleCodesCommand
+import com.github.eltonvs.obd.command.control.TroubleCodesCommand
 import com.github.eltonvs.obd.connection.ObdDeviceConnection
+import java.io.IOException
 
 class ObdHelper(private val bluetoothHelper: BluetoothHelper) {
-    private var obdSocket: ObdDeviceConnection? = null
+    private var obdConnection: ObdDeviceConnection? = null
 
     suspend fun setupObd(deviceAddress: String) {
         val (inputStream, outputStream) = bluetoothHelper.connectToDevice(deviceAddress)
-        obdSocket = ObdDeviceConnection(inputStream, outputStream)
+        obdConnection = ObdDeviceConnection(inputStream, outputStream)
     }
 
     fun getObdDeviceConnection(): ObdDeviceConnection? {
-        return obdSocket
+        return obdConnection
+    }
+
+    suspend fun getDtpCodes() : String
+    {
+        var obdDeviceConnection = obdConnection ?: throw IOException("ObdDeviceConnection is null")
+        return obdDeviceConnection.run(TroubleCodesCommand()).formattedValue
+    }
+    suspend fun getPendingDtpCodes() : String
+    {
+        var obdDeviceConnection = obdConnection ?: throw IOException("ObdDeviceConnection is null")
+        return obdDeviceConnection.run(PendingTroubleCodesCommand()).formattedValue
+    }
+    suspend fun getPermanentDtpCodes() : String
+    {
+        var obdDeviceConnection = obdConnection ?: throw IOException("ObdDeviceConnection is null")
+        return obdDeviceConnection.run(PermanentTroubleCodesCommand()).formattedValue
+    }
+    suspend fun getAllDtpCodes() : String
+    {
+        var responses = mutableListOf<String>()
+        responses.add(getDtpCodes())
+        responses.add(getPendingDtpCodes())
+        responses.add(getPermanentDtpCodes())
+
+        return responses.joinToString(", ")
     }
 
     fun disconnectFromObdDevice() {
